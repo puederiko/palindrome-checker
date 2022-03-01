5de293afd9649d47635b2e3bc178255741d4c692
Frederick Acorda <acorda.fred@gmail.com>
2022-03-01 17:45:23 -0600

    Update script.js
    Added comments and updated message for user.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,43 @@
-function gatherUserInput() {
-  let userInput = document.querySelector("#userInput").value;
-  palindrome(userInput);
-}
-
+// addEventLister to the button for when clicked
 document.querySelector("#submit").addEventListener("click", (event) => {
+  // prevent button click default quirk
   event.preventDefault();
+  // run gatherUserInput()
   gatherUserInput();
 });
 
+function gatherUserInput() {
+  // gather userInput value
+  let userInput = document.querySelector("#userInput").value;
+  // run palindrome() with the userInput as the parameter
+  palindrome(userInput);
+}
+
 function palindrome(str) {
+  // get the element to where we'll populate the message
   let result = document.querySelector("#result");
-  // Set regex to only gather letters and numbers.
+  // set regex to gather only letters and numbers
   let regex = /([a-z]|[0-9])/gim;
-  // Use match() to split the filterred str into an array, then join() it to a single string, then format it toUpperCase(), and save it as a variable.
+  // get filterred str and stringify it to uppercase
   let filterStr = str.match(regex).join("").toUpperCase();
   console.log(filterStr);
-  // split() filterStr into an array, reverse() the array, join() it to a single string, format it toUpperCase(), and save it as a variable.
-  let reverseFilterStr = filterStr.split("").reverse().join("").toUpperCase();
+  //
+  let reverseFilterStr = filterStr.split("").reverse().join("");
   console.log(reverseFilterStr);
   // if filterStr and reverseFilterSTr is strictly equal,
   if (filterStr === reverseFilterStr) {
     // it is a palindrome.
-    result.innerHTML = `"${str}" is a palindrome`;
+    result.innerText = `The parsed letters and/or numbers are
+    ${filterStr}.
+    In reverse, it's
+    ${reverseFilterStr}.
+    Thus, "${str}" is a palindrome`;
   } else {
     // Otherwise, it is not.
-    result.innerHTML = `"${str}" is not a palindrome`;
+    result.innerText = `The parsed letters and/or numbers are
+    ${filterStr}.
+    In reverse, it's
+    ${reverseFilterStr}.
+    Thus, "${str}" is a palindrome`;
   }
 }
