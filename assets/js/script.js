7524fe677c0f8ce03f5ab9fae68b90b7cd49bf4d
Frederick Acorda <acorda.fred@gmail.com>
2022-03-01 15:33:43 -0600

    Update script.js
    Logic now able to take in user input to check.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,29 @@
-// Create a function to take in a string.
+function gatherUserInput() {
+  let userInput = document.querySelector("#userInput").value;
+  palindrome(userInput);
+}
+
+document.querySelector("#submit").addEventListener("click", (event) => {
+  event.preventDefault();
+  gatherUserInput();
+});
+
 function palindrome(str) {
+  let result = document.querySelector("#result");
   // Set regex to only gather letters and numbers.
   let regex = /([a-z]|[0-9])/gim;
   // Use match() to split the filterred str into an array, then join() it to a single string, then format it toUpperCase(), and save it as a variable.
   let filterStr = str.match(regex).join("").toUpperCase();
+  console.log(filterStr);
   // split() filterStr into an array, reverse() the array, join() it to a single string, format it toUpperCase(), and save it as a variable.
   let reverseFilterStr = filterStr.split("").reverse().join("").toUpperCase();
+  console.log(reverseFilterStr);
   // if filterStr and reverseFilterSTr is strictly equal,
   if (filterStr === reverseFilterStr) {
     // it is a palindrome.
-    console.log(`"${str}" is a palindrome`);
+    result.innerHTML = `"${str}" is a palindrome`;
   } else {
     // Otherwise, it is not.
-    console.log(`"${str}" is not a palindrome`);
+    result.innerHTML = `"${str}" is not a palindrome`;
   }
 }
-
-palindrome("neven");
